2250960b8f567a0715de6557e63d7cacdf6df967
adrianopogianeli <adriano.pogianeli@gmail.com>
2018-06-16 19:59:08 -0300

    Add project files.

---
+++ XF.MVVMBasic/XF.MVVMBasic/Model/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using XF.MVVMBasic.ViewModel;
+
+namespace XF.MVVMBasic.Model
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string RM { get; set; }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                App.AlunoVM.OnAdicionarAlunoCMD.AdicionarCanExecuteChanged();
+            }
+        }
+
+        public string Email { get ; set; }
+    }
+
+}
